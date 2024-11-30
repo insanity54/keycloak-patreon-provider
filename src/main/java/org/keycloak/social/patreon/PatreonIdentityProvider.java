bf4774fc4e8ea0807fba558be4d6f923def7391e
Christopher Joule Xenon <chris@grimtech.net>
2024-11-29 20:55:04 -0800

    monkeypatch the currently_entitled_tiers

--- src/main/java/org/keycloak/social/patreon/PatreonIdentityProvider.java
+++ src/main/java/org/keycloak/social/patreon/PatreonIdentityProvider.java
@@ -1,6 +1,8 @@
 package org.keycloak.social.patreon;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.logging.Logger;
 import org.keycloak.broker.oidc.AbstractOAuth2IdentityProvider;
 import org.keycloak.broker.oidc.mappers.AbstractJsonUserAttributeMapper;
@@ -10,11 +12,33 @@ import org.keycloak.broker.provider.util.SimpleHttp;
 import org.keycloak.broker.social.SocialIdentityProvider;
 import org.keycloak.events.EventBuilder;
 import org.keycloak.models.KeycloakSession;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Option;
+import java.util.List;
 
 public class PatreonIdentityProvider extends AbstractOAuth2IdentityProvider<PatreonIdentityProviderConfig>
-        implements SocialIdentityProvider<PatreonIdentityProviderConfig> {
+    implements SocialIdentityProvider<PatreonIdentityProviderConfig> {
 
     private static final Logger log = Logger.getLogger(PatreonIdentityProvider.class);
+
+    private List<String> extractCurrentlyEntitledTiers(JsonNode profile) {
+        try {
+            // Configure JsonPath to handle missing fields gracefully
+            Configuration config = Configuration.defaultConfiguration().addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL);
+
+            // Convert JsonNode to Object for JsonPath parsing
+            // Object jsonDocument = config.jsonProvider().parse(profile.toString());
+            ObjectNode profileObject = (ObjectNode) profile;
+            
+            // config.jsonProvider().parse(profileObject);
+            return JsonPath.read(profileObject, "$.included[?(@.type=='member')].relationships.currently_entitled_tiers.data[*].id");
+
+        } catch (Exception e) {
+            log.warn("Error extracting currently entitled tiers", e);
+            return List.of(); // Return an empty list in case of errors
+        }
+    }
 
     public static final String AUTH_URL = "https://www.patreon.com/oauth2/authorize";
     public static final String TOKEN_URL = "https://www.patreon.com/api/oauth2/token";
@@ -42,8 +66,27 @@ public class PatreonIdentityProvider extends AbstractOAuth2IdentityProvider<Patr
     @Override
     protected BrokeredIdentityContext extractIdentityFromProfile(EventBuilder event, JsonNode profile) {
         BrokeredIdentityContext user = new BrokeredIdentityContext(getJsonProperty(profile.path("data"), "id"), getConfig());
+
         user.setUsername(getJsonProperty(profile.path("data").path("attributes"), "full_name"));
         user.setIdp(this);
+
+        // ((ObjectNode)profile).put("test", "testing123-PatreonIdentityProvider-test-profile-monkeypatch");
+
+
+        // Extract currently entitled tiers using JSONPath
+        List<String> entitledTiers = extractCurrentlyEntitledTiers(profile);
+        // Ensure profile is an ObjectNode before trying to mutate it
+        if (profile instanceof ObjectNode) {
+            ObjectNode objectNode = (ObjectNode) profile;
+
+            // Patch or add a new field (example: "test" with "testing123")
+            objectNode.put("test", "testing123-PatreonIdentityProvider-test-profile-monkeypatch");
+
+            // Optionally add the list of entitled tiers (assuming you're patching the profile with it)
+            if (!entitledTiers.isEmpty()) {
+                objectNode.putPOJO("currently_entitled_tiers", entitledTiers);
+            }
+        }
 
         AbstractJsonUserAttributeMapper.storeUserProfileForMapper(user, profile, getConfig().getAlias());
 
